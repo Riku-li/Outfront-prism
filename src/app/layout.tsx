78ceab6d875bd3c5927bbabc353cfdd45f6b89d3
Riku-li <rickyweringo6@gmail.com>
2025-09-22 16:22:31 -0600

    Outfront Soulutions theme and chatbot created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,24 +4,23 @@ import "./globals.css";
 
 // Configure Inter
 const inter = Inter({
-    subsets: ["latin"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-    title: "Bank Bot",
-    description: "Explora las soluciones inteligentes para selección de talento: IA, automatización y análisis avanzado de CVs para encontrar a los candidatos ideales.",
+  title: "Oufront Solutions",
+  description:
+    "Outfront Solutions' Prism Assistant is a text-based AI chatbot: a simple, web-embedded channel that answers questions directly from Outfront’s documents. Streamlines access to knowledge while staying focused on clarity and reliability.",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
